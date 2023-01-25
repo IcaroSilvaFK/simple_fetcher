66aef1a083814b5c7493a7f601b6616033a57df6
Icaro Vieira <icaro.vieira@impar.com.br>
2023-01-25 14:11:48 -0300

    feat: create toText method

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,56 @@
-const fetcher = {
-  get: async (url: string) => {
-    const response = await fetch(url);
+async function toJson(response: Response) {
+  return response.json();
+}
 
-    return response;
-  },
-  toJson: async (response: Response) => {
-    return response.json();
-  },
-};
+// const fetcher = {
+//   get: async (url: string) => {
+//     const response = await fetch(url);
+
+//     return {
+//       toJson:toJson(response)
+//     }
+//   },
+
+// };
+
+function fetcher() {
+  const methods = {
+    get: async (url: string) => {
+      const response = await fetch(url);
+
+      return {
+        toJson: toJson(response),
+      };
+    },
+    toJson: async (response: Response) => {
+      return await response.json();
+    },
+    toText: async (response: Response) => {
+      return await response.text();
+    },
+  };
+
+  return {
+    get: async (url: string) => {
+      const response = await fetch(url);
+
+      return {
+        toJson: toJson(response),
+      };
+    },
+    toJson: async (response: Response) => {
+      return await response.json();
+    },
+    toText: async (response: Response) => {
+      return await response.text();
+    },
+  };
+}
+
+const test = fetcher();
+
+(async () => {
+  const response = await (
+    await test.get('https://api.github.com/users/IcaroSilvaFK')
+  ).toJson;
+})();
